9c85c9802647d93028ba31b10245e6e9546969e1
Daniele D'Orazio <d.dorazio96@gmail.com>
2018-10-01 22:14:06 +0200

    use wasm-pack build instead of init

--- WASMbindgenAsset.js
+++ WASMbindgenAsset.js
@@ -35,7 +35,7 @@ class WASMbindgenAsset extends Asset {
   }
 
   async crateTypeCheck(cargoConfig) {
-    if (!cargoConfig.lib || 
+    if (!cargoConfig.lib ||
         !Array.isArray(cargoConfig.lib['crate-type']) ||
         !cargoConfig.lib['crate-type'].includes('cdylib')) {
       throw 'The `crate-type` in Cargo.toml should be `cdylib`'
@@ -59,7 +59,7 @@ class WASMbindgenAsset extends Asset {
 
     const build_result = {
       cargoDir
-    } 
+    }
 
     if (has_wasm_pack) {
       Object.assign(build_result, await this.wasmPackBuild(cargoConfig, cargoDir, has_cargo && has_wasmbindgen))
@@ -77,7 +77,7 @@ class WASMbindgenAsset extends Asset {
   }
 
   async wasmPackBuild(cargoConfig, cargoDir, has_deps) {
-    const args = has_deps ? ['init', '-m', 'no-install'] : ['init']
+    const args = has_deps ? ['build', '-m', 'no-install'] : ['build']
     await exec('wasm-pack', args, {cwd: cargoDir})
 
     return {
